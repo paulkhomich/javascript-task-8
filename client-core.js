0f0b74309ae7e975660d7e6f4063f1201ebdd344
Paul Khomich <paulkhomich@outlook.com>
2017-12-06 16:38:03 +0500

    добавил промисы

--- client-core.js
+++ client-core.js
@@ -1,107 +1,92 @@
 'use strict';
 
 const http = require('http');
-const chalk = require('chalk');
 
 module.exports.execute = execute;
 module.exports.isStar = false;
 
 function execute() {
-    // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
-    const command = args[2];
-    let argsParams = args.slice(3).join(' ');
-    let from = argsParams.match(/--from(=|\s)\S+/i);
-    let to = argsParams.match(/--to(=|\s)\S+/i);
-    let text = argsParams.match(/--text(=|\s)\S+/i);
-    from = from ? from[0].replace('--from', '').slice(1) : null;
-    to = to ? to[0].replace('--to', '').slice(1) : null;
-    text = text ? text[0].replace('--text', '').slice(1) : null;
+    return new Promise(async (resolve, reject) => {
+        // Внутри этой функции нужно получить и обработать аргументы командной строки
+        const args = process.argv;
+        const command = args[2];
+        let argsParams = args.slice(3).join(' ');
+        let from = argsParams.match(/--from(=|\s)\S+/i);
+        let to = argsParams.match(/--to(=|\s)\S+/i);
+        let text = argsParams.match(/--text(=|\s)\S+/i);
+        from = from ? from[0].replace('--from', '').slice(1) : null;
+        to = to ? to[0].replace('--to', '').slice(1) : null;
+        text = text ? text[0].replace('--text', '').slice(1) : null;
 
-    if (command === 'list') {
-        getList(from, to);
-    } else if (command === 'send') {
-        sendMessage(from, to, text);
-    } else {
-        console.info('command not found');
-    }
+        let body = [];
+        if (command === 'list') {
+            body = await getList(from, to);
+        } else if (command === 'send') {
+            body = await sendMessage(from, to, text);
+        } else {
+            reject('command not found');
+        }
 
-    return Promise.resolve('');
+        resolve(body);
+    });
 }
 
 function getList(from, to) {
-    let path = '/messages?';
-    path += from ? 'from=' + encodeURIComponent(from) + '&' : '';
-    path += to ? 'to=' + encodeURIComponent(to) : '';
+    return new Promise((resolve) => {
+        let path = '/messages?';
+        path += from ? 'from=' + encodeURIComponent(from) + '&' : '';
+        path += to ? 'to=' + encodeURIComponent(to) : '';
 
-    const req = http.request({
-        hostname: 'localhost',
-        method: 'GET',
-        path: path,
-        port: 8080
-    });
-
-    req.on('response', response => {
-        let body = '';
-
-        response.on('data', chunk => {
-            body += chunk;
+        const req = http.request({
+            hostname: 'localhost',
+            method: 'GET',
+            path: path,
+            port: 8080
         });
 
-        response.on('end', () => {
-            prettyMessage(body);
-        });
-    });
+        req.on('response', response => {
+            let body = '';
 
-    req.end();
+            response.on('data', chunk => {
+                body += chunk;
+            });
+
+            response.on('end', () => {
+                resolve(body);
+            });
+        });
+
+        req.end();
+    });
 }
 
 function sendMessage(from, to, text) {
-    let path = '/messages?';
-    path += from ? 'from=' + encodeURIComponent(from) + '&' : '';
-    path += to ? 'to=' + encodeURIComponent(to) : '';
-    text = encodeURIComponent(text);
+    return new Promise((resolve) => {
+        let path = '/messages?';
+        path += from ? 'from=' + encodeURIComponent(from) + '&' : '';
+        path += to ? 'to=' + encodeURIComponent(to) : '';
+        text = encodeURIComponent(text);
 
-    const req = http.request({
-        hostname: 'localhost',
-        method: 'POST',
-        path: path,
-        headers: { 'text': text },
-        port: 8080
-    });
-
-    req.on('response', response => {
-        let body = '';
-
-        response.on('data', chunk => {
-            body += chunk;
+        const req = http.request({
+            hostname: 'localhost',
+            method: 'POST',
+            path: path,
+            headers: { 'text': text },
+            port: 8080
         });
 
-        response.on('end', () => {
-            prettyMessage(body);
+        req.on('response', response => {
+            let body = '';
+
+            response.on('data', chunk => {
+                body += chunk;
+            });
+
+            response.on('end', () => {
+                resolve(body);
+            });
         });
-    });
 
-    req.end();
-}
-
-function prettyMessage(body) {
-    body = JSON.parse(body);
-    body.forEach(mes => {
-        let from = mes.from;
-        let to = mes.to;
-        let text = mes.text;
-        if (from) {
-            console.info(chalk.hex('#F00')('FROM') + ': ' + from);
-        }
-        if (to) {
-            console.info(chalk.hex('#F00')('TO') + ': ' + to);
-        }
-        if (text) {
-            console.info(chalk.hex('#0F0')('TEXT') + ': ' + text);
-        }
-        if (body.indexOf(mes) + 1 !== body.length) {
-            console.info('');
-        }
+        req.end();
     });
 }
--- client.js
+++ client.js
@@ -2,10 +2,31 @@
 'use strict';
 
 const { execute } = require('./client-core');
+const chalk = require('chalk');
 
 module.exports.isStar = false;
 
 execute()
-    .then()
-    .catch();
+    .then(prettyMessage)
+    .catch(console.error);
 
+function prettyMessage(body) {
+    body = JSON.parse(body);
+    body.forEach(mes => {
+        let from = mes.from;
+        let to = mes.to;
+        let text = mes.text;
+        if (from) {
+            console.info(chalk.hex('#F00')('FROM') + ': ' + from);
+        }
+        if (to) {
+            console.info(chalk.hex('#F00')('TO') + ': ' + to);
+        }
+        if (text) {
+            console.info(chalk.hex('#0F0')('TEXT') + ': ' + text);
+        }
+        if (body.indexOf(mes) + 1 !== body.length) {
+            console.info('');
+        }
+    });
+}
