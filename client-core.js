27deaa45ba027b57f20d920720565b70f66592c9
Paul Khomich <paulkhomich@outlook.com>
2017-12-06 15:42:16 +0500

    сервер 0.1

--- client-core.js
+++ client-core.js
@@ -1,11 +1,107 @@
 'use strict';
 
+const http = require('http');
+const chalk = require('chalk');
+
 module.exports.execute = execute;
-module.exports.isStar = true;
+module.exports.isStar = false;
 
 function execute() {
     // Внутри этой функции нужно получить и обработать аргументы командной строки
     const args = process.argv;
+    const command = args[2];
+    let argsParams = args.slice(3).join(' ');
+    let from = argsParams.match(/--from(=|\s)\S+/i);
+    let to = argsParams.match(/--to(=|\s)\S+/i);
+    let text = argsParams.match(/--text(=|\s)\S+/i);
+    from = from ? from[0].replace('--from', '').slice(1) : null;
+    to = to ? to[0].replace('--to', '').slice(1) : null;
+    text = text ? text[0].replace('--text', '').slice(1) : null;
+
+    if (command === 'list') {
+        getList(from, to);
+    } else if (command === 'send') {
+        sendMessage(from, to, text);
+    } else {
+        console.info('command not found');
+    }
 
     return Promise.resolve('Это строка будет выведена на консоль');
+}
+
+function getList(from, to) {
+    let path = '/messages?';
+    path += from ? 'from=' + encodeURIComponent(from) + '&' : '';
+    path += to ? 'to=' + encodeURIComponent(to) : '';
+
+    const req = http.request({
+        hostname: 'localhost',
+        method: 'GET',
+        path: path,
+        port: 8080
+    });
+
+    req.on('response', response => {
+        let body = '';
+
+        response.on('data', chunk => {
+            body += chunk;
+        });
+
+        response.on('end', () => {
+            prettyMessage(body);
+        });
+    });
+
+    req.end();
+}
+
+function sendMessage(from, to, text) {
+    let path = '/messages?';
+    path += from ? 'from=' + encodeURIComponent(from) + '&' : '';
+    path += to ? 'to=' + encodeURIComponent(to) : '';
+    text = encodeURIComponent(text);
+
+    const req = http.request({
+        hostname: 'localhost',
+        method: 'POST',
+        path: path,
+        headers: { 'text': text },
+        port: 8080
+    });
+
+    req.on('response', response => {
+        let body = '';
+
+        response.on('data', chunk => {
+            body += chunk;
+        });
+
+        response.on('end', () => {
+            prettyMessage(body);
+        });
+    });
+
+    req.end();
+}
+
+function prettyMessage(body) {
+    body = JSON.parse(body);
+    body.forEach(mes => {
+        let from = mes.from;
+        let to = mes.to;
+        let text = mes.text;
+        if (from) {
+            console.info(chalk.hex('#F00')('FROM') + ': ' + from);
+        }
+        if (to) {
+            console.info(chalk.hex('#F00')('TO') + ': ' + to);
+        }
+        if (text) {
+            console.info(chalk.hex('#0F0')('TEXT') + ': ' + text);
+        }
+        if (body.indexOf(mes) + 1 !== body.length) {
+            console.info('');
+        }
+    });
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,43 @@
 'use strict';
 
 const http = require('http');
+const { parse: parseUrl } = require('url');
+const { parse: parseQuery } = require('querystring');
 
 const server = http.createServer();
 
+let DB = [];
+
 server.on('request', (req, res) => {
+    const { query } = parseUrl(req.url);
+    const { from, to } = parseQuery(query);
+    const body = {};
+    body.text = decodeURIComponent(req.headers.text);
     // Тут нужно обработать запрос
+    res.setHeader('content-type', 'application/json');
+    if (req.url.startsWith('/messages') && req.method === 'GET') {
+        res.write(JSON.stringify(createLocalDB(from, to)));
+    } else if (req.url.startsWith('/messages') && req.method === 'POST') {
+        if (from) {
+            body.from = from;
+        }
+        if (to) {
+            body.to = to;
+        }
+        DB.push(body);
+        res.write(JSON.stringify([body]));
+    } else {
+        res.statusCode = 404;
+    }
     res.end();
 });
 
 module.exports = server;
+
+function createLocalDB(from, to) {
+    let localDB = DB;
+    localDB = from ? localDB.filter(e => e.from === from) : localDB;
+    localDB = to ? localDB.filter(e => e.to === to) : localDB;
+
+    return localDB;
+}
